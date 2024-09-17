2329d9dede68181cb2c45e5c693358b86719714d
kjijo <kjijo@deloitte.com>
2024-09-17 13:38:52 -0500

    setup for vacancy data

--- app/vacancies/serializers.py
+++ app/vacancies/serializers.py
@@ -1,12 +1,17 @@
+import json
 import logging
 
 from rest_framework import serializers
 
+from external.models import Job
+from external.serializers import JobSerializer
 from vacancies.models import Vacancy
 
 logger = logging.getLogger(__name__)
 
 class VacancySerializer(serializers.ModelSerializer):
+
+    job = JobSerializer(required=False, allow_null=True)
     class Meta:
         model = Vacancy
         fields = ["vacancy_key", "vacancy_key_hash", "JobTitle", "JobTitle",
@@ -16,4 +21,22 @@ class VacancySerializer(serializers.ModelSerializer):
                   "JobPostingID", "JobPostingSite", "OrganizationName", "PositionEndDate",
                   "PositionStartDate", "PublicationStartDate", "PromotionPotential",
                   "ProviderName", "Qualifications", "Relocation", "TravelRequired", "UIDUnitIdentifiers",
-                  "WorkSchedule"]
+                  "WorkSchedule", "job"]
+        
+    def create(self, validated_data):
+
+        job_data = {}
+        if 'job' in validated_data:
+            job_data = validated_data.pop('job')
+
+        vacancy = Vacancy.objects.create(**validated_data)
+
+        if job_data:
+
+            job, c = Job.objects.get_or_create(reference=job_data["reference"],
+                                                     defaults = job_data)
+            vacancy.job = job
+
+            vacancy.save()
+
+        return vacancy
