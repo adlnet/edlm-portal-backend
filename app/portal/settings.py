759597d69ded6b2169bbc320cd61606af233fcc7
KarenAJ <kjijo@deloitte.com>
2024-10-23 10:18:06 -0500

    Update settings.py -CORS

--- app/portal/settings.py
+++ app/portal/settings.py
@@ -136,6 +136,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Add CORS Variables
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
