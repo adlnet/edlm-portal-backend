7f1c35bb4cda60c41b6592a11155a2d6ea73d0e1
JDTobin <90791836+JDTobin@users.noreply.github.com>
2025-01-16 11:33:28 -0500

    add more tests

--- app/external/tests/test_utils_unit.py
+++ app/external/tests/test_utils_unit.py
@@ -1,5 +1,5 @@
 import json
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 from django.test import tag
 
@@ -7,9 +7,11 @@ from configuration.models import Configuration
 from external.models import Course
 from external.utils.eccr_utils import (get_eccr_data_api_url,
                                        get_eccr_search_api_url)
-from external.utils.xds_utils import (format_metadata, get_course_name,
-                                      get_courses_api_url, metadata_to_target,
-                                      save_courses)
+from external.utils.xds_utils import (TokenAuth, format_metadata,
+                                      get_course_name, get_courses_api_url,
+                                      get_xds_experience,
+                                      handle_unauthenticated_user,
+                                      metadata_to_target, save_courses)
 
 from .test_setup import TestSetUp
 
@@ -126,3 +128,57 @@ class UtilsTests(TestSetUp):
         ret = get_eccr_data_api_url()
 
         self.assertEqual(ret, expected)
+
+    def test_handle_unauthenticated_user(self):
+        """Test that 401 response is returned"""
+        expected = 401
+        actual = handle_unauthenticated_user()
+
+        self.assertEqual(actual.status_code, expected)
+
+    def test_token_auth(self):
+        """Test that Token Auth is created correctly"""
+        token = "abc"
+        expected = "abc"
+        actual = TokenAuth(token)
+        req = Mock()
+        req.headers = {}
+        actual(req)
+
+        self.assertEqual(actual.token, expected)
+        self.assertIn('Authorization', req.headers)
+        self.assertIn(token, req.headers['Authorization'])
+        self.assertIn('Bearer', req.headers['Authorization'])
+
+    def test_get_xds_exp(self):
+        """Test that xds request is correctly formatted"""
+        exp_id = "xyz"
+        expected = None
+        xds_api = "https://example.com"
+        conf = Configuration(target_xds_api=xds_api)
+        conf.save()
+        with patch('external.utils.xds_utils.requests') as req:
+            req.get.return_value = None
+            actual = get_xds_experience(exp_id)
+
+            self.assertEqual(actual, expected)
+            req.get.assert_called_once()
+            self.assertIn(exp_id, req.get.call_args[0][0])
+
+    def test_get_xds_exp_with_auth(self):
+        """Test that xds request is correctly formatted"""
+        token = "abc"
+        auth = TokenAuth(token)
+        exp_id = "xyz"
+        expected = None
+        xds_api = "https://example.com"
+        conf = Configuration(target_xds_api=xds_api)
+        conf.save()
+        with patch('external.utils.xds_utils.requests') as req:
+            req.get.return_value = None
+            actual = get_xds_experience(exp_id, auth=auth)
+
+            self.assertEqual(actual, expected)
+            req.get.assert_called_once()
+            self.assertIn(exp_id, req.get.call_args[0][0])
+            self.assertEqual(auth, req.get.call_args[1]['auth'])
