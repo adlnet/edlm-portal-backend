270a967761ab90e0a4da7f8c44bce7c4b954c314
cscholer591 <cscholer@gps.deloitte.com>
2026-01-29 11:55:59 -0500

    config info added to readme and settings.py variables fixed

--- app/portal/settings.py
+++ app/portal/settings.py
@@ -18,21 +18,19 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oyg-*!=r_t3za4ii7(l(tk%8+ubb3h&yg+!z-4f@+*3m8p4_60'
+SECRET_KEY = os.environ.get('SECRET_KEY_VAL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ.get("HOSTS")]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
